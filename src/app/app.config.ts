8c3107180e165afff29b6475e98a72916a709ab3
ewgenij87snwork <ewgenij87snwork@gmail.com>
2024-06-22 20:16:26 +0300

    Add | Angular Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
 import {provideCharts, withDefaultRegisterables} from "ng2-charts";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideCharts(withDefaultRegisterables())
+    provideCharts(withDefaultRegisterables()), provideAnimationsAsync()
   ]
 };
